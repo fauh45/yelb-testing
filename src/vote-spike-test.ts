c82d0bd92114902a5ff4b1ee6c7497b0ff210cf2
Fauzan Lubis <fauzanfauh45@gmail.com>
2022-04-21 21:31:33 +0700

    Change Test to Spike

--- src/vote-spike-test.ts
+++ src/vote-spike-test.ts
@@ -12,13 +12,10 @@ const chooseRandomChoice = () => {
 
 export const options: Options = {
   stages: [
-    { duration: "1m", target: 10 },
-    { duration: "5m", target: 25 },
-    { duration: "5m", target: 50 },
-    { duration: "2m", target: 100 },
-    { duration: "5m", target: 150 },
-    { duration: "2m", target: 200 },
-    { duration: "5m", target: 200 },
+    { duration: "2m", target: 25 },
+    { duration: "2m", target: 50 },
+    { duration: "5m", target: 2000 },
+    { duration: "5m", target: 2500 },
     { duration: "10m", target: 0 },
   ],
 };
